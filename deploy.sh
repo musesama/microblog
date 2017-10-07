b7c11dc7d97939f9eb60b94e33f21a19a9cd3212
Xiangyu Li <karlcen.li@gmail.com>
2017-10-07 08:26:12 +0000

    ADD deploy.sh and MDY rel respectively

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# TODO: consider adapt with systemd
+export PORT=8000
+export MIX_ENV=prod
+DIR=$1
+
+if [ ! -d "$DIR" ]; then
+	  printf "Usage: ./deploy.sh <path>\n"
+		  exit
+		fi
+
+		echo "Deploy to [$DIR]"
+
+		mix deps.get
+		(cd assets && npm install)
+		(cd assets && ./node_modules/brunch/bin/brunch b -p)
+		mix phx.digest
+		mix release --env=prod
+
+		$DIR/bin/microblog stop || true
+
+		mix ecto.migrate
+
+		SRC=`pwd`
+		(cd $DIR && tar xf $SRC/_build/prod/rel/microblog/releases/0.0.1/microblog.tar.gz)
+
+		$DIR/bin/microblog start
